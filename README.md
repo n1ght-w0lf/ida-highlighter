941b5d7de85e50ea8aa837cb7122b4f4d496c3c0
Abdallah Elshinbary <abdallahelking82@gmail.com>
2020-01-13 21:54:01 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ida-highlighter
 A simple script for highlighting important instructions in IDA Pro
+
--- ida_highlighter.py
+++ ida_highlighter.py
@@ -12,46 +12,46 @@ XOR_COLOR = 0x8a0a8a
 ANITDEBUG_COLOR = 0x0a0a8a
 
 def highlight_insn(ea, color, comment="", repeatable=0):
-	idc.set_color(ea, CIC_ITEM , color)
-	idc.set_cmt(ea, comment, repeatable)
+  idc.set_color(ea, CIC_ITEM , color)
+  idc.set_cmt(ea, comment, repeatable)
 
 def highlight_anti_debug():
-	funs = ["IsDebuggerPresent", "CheckRemoteDebuggerPresent", "NtQueryInformationProcess", 
-			"OutputDebugString", "QueryPerformanceCounter", "GetTickCount"]
-	for func in funs:
-		func_ea = get_name_ea_simple(func)
-		xrefs = list(idautils.CodeRefsTo(func_ea, 0))
-		for ea in xrefs:
-			highlight_insn(ea, ANITDEBUG_COLOR, "Possible Anti-Debugging")
+  funs = ["IsDebuggerPresent", "CheckRemoteDebuggerPresent", "NtQueryInformationProcess", 
+      "OutputDebugString", "QueryPerformanceCounter", "GetTickCount"]
+  for func in funs:
+    func_ea = get_name_ea_simple(func)
+    xrefs = list(idautils.CodeRefsTo(func_ea, 0))
+    for ea in xrefs:
+      highlight_insn(ea, ANITDEBUG_COLOR, "Possible Anti-Debugging")
 
 def main():
-	for func in idautils.Functions():
-		#get function flags
-		flags = idc.get_func_attr(func, FUNCATTR_FLAGS)
-		
-		# skip library & thunk functions
-		if flags & FUNC_LIB or flags & FUNC_THUNK:
-			continue
+  for func in idautils.Functions():
+    #get function flags
+    flags = idc.get_func_attr(func, FUNCATTR_FLAGS)
+    
+    # skip library & thunk functions
+    if flags & FUNC_LIB or flags & FUNC_THUNK:
+      continue
 
-		dism_addr = list(idautils.FuncItems(func))
-		for ea in dism_addr:
-			mnem = idc.print_insn_mnem(ea)
+    dism_addr = list(idautils.FuncItems(func))
+    for ea in dism_addr:
+      mnem = idc.print_insn_mnem(ea)
 
-			if mnem == "call":
-				highlight_insn(ea, CALL_COLOR)
+      if mnem == "call":
+        highlight_insn(ea, CALL_COLOR)
 
-			elif mnem == "rdtsc":
-				highlight_insn(ea, ANITDEBUG_COLOR, "Possible Anti-Debugging")
+      elif mnem == "rdtsc":
+        highlight_insn(ea, ANITDEBUG_COLOR, "Possible Anti-Debugging")
 
-			elif mnem == "xor":
-				op1 = idc.print_operand(ea, 0)
-				op2 = idc.print_operand(ea, 1)
-				if op1 == op2:
-					highlight_insn(ea, DEFAULT_COLOR, "{} = 0".format(op1))
-				else:
-					highlight_insn(ea, XOR_COLOR, "Possible enc/dec")
+      elif mnem == "xor":
+        op1 = idc.print_operand(ea, 0)
+        op2 = idc.print_operand(ea, 1)
+        if op1 == op2:
+          highlight_insn(ea, DEFAULT_COLOR, "{} = 0".format(op1))
+        else:
+          highlight_insn(ea, XOR_COLOR, "Possible enc/dec")
 
-	highlight_anti_debug()
+  highlight_anti_debug()
 
 if __name__ == "__main__":
-	main()
+  main()
